0d7125ff3ebe29a1b27537495e03eb2138866cec
phanisumanthpentapati <sumanthpentapati1@gmail.com>
2026-07-18 21:14:02 +0530

    Fix API base URL

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://placement-portal-x5j7.onrender.com",
+  baseURL: "https://placement-portal-x5j7.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
