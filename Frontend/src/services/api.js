262d847a4fb47a4a35dadfe9af505a2512f4eea8
phanisumanthpentapati <sumanthpentapati1@gmail.com>
2026-07-18 21:02:55 +0530

    Update backend API URL

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://placement-portal-x5j7.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
